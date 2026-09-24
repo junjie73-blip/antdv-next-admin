861c0c6855fe23c3a42eaecc45d469a88016bb29
zy <zoujunjie73@gmail.com>
2026-09-24 11:14:02 +0800

    fix: 🐛 删除不存在 的引入地址

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,6 +1,5 @@
-export { useCRUD } from './useCRUD'
-export { usePermission } from './web/permission'
-export { useSSE } from './web/sse'
-export { useTheme } from './web/useTheme'
-export { useWatermark } from './web/useWatermark'
-export * from './web/useWelcomeNotification'
+export { useCRUD } from "./useCRUD";
+export { usePermission } from "./web/permission";
+export { useSSE } from "./web/sse";
+export { useWatermark } from "./web/useWatermark";
+export * from "./web/useWelcomeNotification";
